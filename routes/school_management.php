b32bd23fb4bac7e082ef68273e467fef862f3ba9
NavieaPc 8 <navieadevelopers@gmail.com>
2024-05-13 08:52:46 +0600

    ..

--- routes/school_management.php
+++ routes/school_management.php
@@ -176,16 +176,6 @@ Route::prefix('admission')->middleware(['auth:admin', 'adminCheck:0'])->group( f
 });
 
 
-Route::get('/get/group/{id}', [AdmissionController::class, 'getGroup']);
-
-
-
-// Route::get('/get-groups/{class_id}', [AdmissionController::class, 'getGroups']);
-// Route::get('/get-sections/{class_id}', [AdmissionController::class, 'getSections']);
-
-
-
-
 
 
 // -------------------------------------
@@ -203,10 +193,6 @@ Route::get('/get/group/{id}', [AdmissionController::class, 'getGroup']);
     // Route::get('/status/{id}', [AdmissionAdmissionController::class, 'status'])->name('admin.admission.status');
 // });
 
-Route::get('get-groups/{class_id}', [AdmissionController::class, 'getGroups']);
-Route::get('get-sections/{class_id}', [AdmissionController::class, 'getSections']);
-
-// Route::get('/get-groups/{class_id}', 'App\Http\Controllers\ClassController@getGroups');
-// Route::get('/get-sections/{class_id}', 'App\Http\Controllers\ClassController@getSections');
 
 
+Route::get('/get/group/{id}', [AdmissionController::class, 'getGroup']);
