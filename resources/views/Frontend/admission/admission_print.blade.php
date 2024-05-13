68fd2de6725c59b336fdec107e8c7863cf4d82eb
navieasoft <navieadevelopers@gmail.com>
2024-05-13 17:58:51 +0600

    ..

--- resources/views/Frontend/admission/admission_print.blade.php
+++ resources/views/Frontend/admission/admission_print.blade.php
@@ -1,83 +1,178 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  {{-- <title>Class Routine</title> --}}
-  <!-- Bootstrap CSS -->
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-v8FpMN7PZIWiDRfM7czPpOVCKaHcJK4jO3kI+KTO9vMjCExH0EeZ3rAaxiylpDcT" crossorigin="anonymous">
-  <style>
-    /* Additional custom styles */
-    .school-name {
-      text-align: center;
-      margin-bottom: 30px;
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Application Form</title>
+<style>
+    body {
+        font-family: Arial, sans-serif;
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
     }
-    .class-routine {
-      margin-bottom: 50px;
+
+    .container {
+        max-width: 800px;
+        margin: 50px auto;
+        padding: 20px;
     }
-    .class-routine table {
-      width: 100%;
-      border-collapse: collapse;
+
+    .row {
+        display: flex;
+        flex-wrap: wrap;
+        margin-bottom: 10px;
     }
-    .class-routine th, .class-routine td {
-      border: 1px solid #ccc;
-      padding: 8px;
-      text-align: center;
+
+    .column {
+        flex: 1;
+        margin-right: 10px;
     }
-    .class-routine th {
-      background-color: #f2f2f2;
+
+    .column:last-child {
+        margin-right: 0;
     }
-  </style>
+
+    label {
+        display: block;
+        margin-bottom: 5px;
+        font-weight: bold;
+    }
+
+</style>
 </head>
 <body>
 
 <div class="container">
-
-    <div class="col-md-12 px-4 pt-2 pb-4 px-sm-5 pb-sm-5 pt-md-5" >
-        <h3 class="text-center">Admission Form</h3>
-        
-            <div class="row ">
-                <div class="col-sm-4" >
-                    <div class="mt-1 mr-2" style="position:relative;box-shadow: 0px 0px 1px 1px;width: 180px;">
-                        <img class="display-upload-img" style="width: 180px;height: 200px;" src="{{ $details->image_show }}" alt="">
-                        <input type="file" name="image" class="form-control upload-img" placeholder="Enter Activity Image" style="position: absolute;top: 0;opacity: 0;height: 100%;">
-                    </div>
-                </div>
-            </div>
-
-            <div class="row mt-4">
-                <hr>
-
-                <div class="col-sm-4">
-                    <label class=" form-control-label"><b>Class: </b></label>
-                        <p >{{ @$details->class->name }}</p>
-                </div>
-
-                <div class="col-sm-4">
-                <label class=" form-control-label"><b>Group: </b></label>
-                <p >{{ @$details->group->name }}</p>
-                </div>
-
-                <div class="col-sm-4">
-                <label class=" form-control-label"><b>Fees: </b></label>
-                <p >{{ @$details->feeManagement->fee->particular_name }} - ({{ @$details->feeManagement->fee_amount }})</p>
-                </div>
-                
-            </div>
-
+  <div class="row ">
+    <div class="col-sm-4" >
+        <div class="mt-1 mr-2" style="position:relative;box-shadow: 0px 0px 1px 1px;width: 180px;">
+            <img class="display-upload-img" style="width: 180px;height: 200px;" src="{{ $details->image_show }}" alt="">
+            <input type="file" name="image" class="form-control upload-img" placeholder="Enter Activity Image" style="position: absolute;top: 0;opacity: 0;height: 100%;">
+        </div>
     </div>
 </div>
+    <h2>Application Form</h2>
+        <div class="row">
+            <div class="column">
+                <label for="firstName">Class:</label>
+                <p>{{ @$details->class->name }}</p>
+            </div>
+            <div class="column">
+                <label for="lastName">Group:</label>
+                <p>{{ @$details->group->name }}</p>
+            </div>
+            <div class="column">
+                <label for="email">Fees:</label>
+                <p>{{ @$details->feeManagement->fee->particular_name }} - ({{ @$details->feeManagement->fee_amount }})</p>
+            </div>
+        </div>
 
-<!-- Bootstrap JS -->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-k3A1LmfFiBCL0h9pzyvLWNs/zFwMIGg/2IQXO48JAU0MTrvYV4R8aG3g7AgL73n9" crossorigin="anonymous"></script>
-<script>
-    window.print();
-    window.onafterprint = back;
+        <h2>Student Information</h2>
+        <div class="row">
+            <div class="column">
+                <label for="phone">Student Name :</label>
+                <p>NIaz</p>
+            </div>
+            <div class="column">
+                <label for="dob">Date of Birth :</label>
+                <p>NIaz</p>
+            </div>
+            <div class="column">
+                <label for="gender">Student Phone :</label>
+                <p>NIaz</p>
+            </div>
+        </div>
 
-    function back() {
-        window.close();
-        window.history.back();
-    }
-</script>
+        <div class="row">
+          <div class="column">
+              <label for="phone">Student Email :</label>
+              <p>NIaz</p>
+          </div>
+          <div class="column">
+              <label for="dob">Student NID/Birth Certificate :</label>
+              <p>NIaz</p>
+          </div>
+          <div class="column">
+              <label for="gender">Father Name :</label>
+              <p>NIaz</p>
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="column">
+              <label for="phone">Father Occupation :</label>
+              <p>NIaz</p>
+          </div>
+          <div class="column">
+              <label for="dob">Father Phone :</label>
+              <p>NIaz</p>
+          </div>
+          <div class="column">
+              <label for="gender">Father NID :</label>
+              <p>NIaz</p>
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="column">
+              <label for="phone">Father Mother Name :</label>
+              <p>NIaz</p>
+          </div>
+          <div class="column">
+              <label for="dob">Mother Occupation :</label>
+              <p>NIaz</p>
+          </div>
+          <div class="column">
+              <label for="gender">Mother Phone :</label>
+              <p>NIaz</p>
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="column">
+              <label for="phone">Yearly Income :</label>
+              <p>NIaz</p>
+          </div>
+        </div>
+
+
+        
+        <h2>Present Address</h2>
+        <div class="row">
+            <div class="column">
+                <label for="phone">Phone:</label>
+                <p>NIaz</p>
+            </div>
+            <div class="column">
+                <label for="dob">Date of Birth:</label>
+                <p>NIaz</p>
+            </div>
+            <div class="column">
+                <label for="gender">Gender:</label>
+                <p>NIaz</p>
+            </div>
+        </div>
+
+        <h2>Permanent Address</h2>
+        <div class="row">
+            <div class="column">
+                <label for="phone">Phone:</label>
+                <p>NIaz</p>
+            </div>
+            <div class="column">
+                <label for="dob">Date of Birth:</label>
+                <p>NIaz</p>
+            </div>
+            <div class="column">
+                <label for="gender">Gender:</label>
+                <p>NIaz</p>
+            </div>
+        </div>
+
+        
+        
+</div>
+
 </body>
 </html>
