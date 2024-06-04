47185593ff4d4ab298ddcdb35aa1566cae4ccf7e
NavieaPc 8 <navieadevelopers@gmail.com>
2024-06-04 16:01:32 +0600

    ..

--- resources/views/user/notice/notice.blade.php
+++ resources/views/user/notice/notice.blade.php
@@ -29,29 +29,17 @@
         <di class="col-md-12">
             <div class="row">
                 @foreach ($notices as $notice)
-               
-                <div class="col-md-4">
-                    <div class="card card-body" style="height: 100%">
-                        <p class="text-center" style="color: black"><b>
-                        <a href="{{ route('frontend.notice_details',$notice->id) }}">
-                                @if ($notice->type == 'daily')
-                                    Daily Notice
-                                @elseif ($notice->type == 'monthly')
-                                    Monthly Notice
-                                @elseif ($notice->type == 'yearly')
-                                    Yearly Notice
-                                @elseif ($notice->type == 'instant')
-                                    Istant Notice
-                                @endif
-                            </b></p>
-                            <h5 class="mb-3" style="color: black">Title: {{ $notice->name }}</h5>
-                            <p class="" style="color: black; text-align: justify;"><b>Description:</b> {{ $notice->description }}</p>
-                            <p><a href="{{ route('frontend.notice_pdf_download',$notice->id) }}">click here to download the PDF file.</a></p>
-                            <p style="color: black"><b>Created at:</b> {{ \Carbon\Carbon::parse($notice->created_at)->isoFormat('Do MMM YYYY') }}</p>
-                       </a>
+                    <div class="col-md-4 mt-3">
+                        <div class="card card-body" style="height: 100%">
+                            <a href="{{ route('frontend.notice_list') }}">
+                                <p class="text-center" style="color: black"><b>{{ $notice->noticeType->name }}</b></p>
+                                <h5 class="mb-3" style="color: black">Title: {{ $notice->name }}</h5>
+                                {{-- <p class="" style="color: black; text-align: justify;"><b>Description:</b> {{ $notice->description }}</p> --}}
+                                <p><a href="{{ route('frontend.notice_pdf_download',$notice->id) }}">click here to download the PDF file.</a></p>
+                                <p style="color: black"><b>Created at:</b> {{ \Carbon\Carbon::parse($notice->created_at)->isoFormat('Do MMM YYYY') }}</p>
+                            </a>
+                        </div>
                     </div>
-                </div>
-           
                 @endforeach
             </div>
         </di>
