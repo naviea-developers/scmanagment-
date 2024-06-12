5692a61236fe438b8fe979a5d872f2cb8bcce222
NavieaPc 8 <navieadevelopers@gmail.com>
2024-06-12 15:00:02 +0600

    ..

--- app/Http/Controllers/Backend/School_Management/ExamRoutine/ExamRoutineController.php
+++ app/Http/Controllers/Backend/School_Management/ExamRoutine/ExamRoutineController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\ExamSchedule;
 use App\Models\Examination;
+use App\Models\Session;
 use App\Models\Tp_option;
 
 class ExamRoutineController extends Controller
@@ -13,16 +14,23 @@ class ExamRoutineController extends Controller
 
     public function index(){
         $data['examinations']=Examination::where('status','1')->orderBy('id', 'desc')->get();
+        $data['sessions']=Session::where('status', 1)->orderBy('id', 'desc')->get(); 
         return view('Backend.school_management.exam_routine.index',$data);
     }
     
 
-     //ajax get Exam Schedule Routine
-     public function getExamRoutine($id){
+    public function getExamRoutine($examination_id, $session_id) {
         $data['tpOption'] =Tp_option::where('option_name', 'theme_option_header')->first();
-        $data['examSchedules']= ExamSchedule::where('status','1')->where("examination_id",$id)->get();
+
+        if($examination_id && $session_id){
+        $data['examSchedules']= ExamSchedule::where('examination_id', $examination_id)
+                                  ->whereHas('examination', function($query) use ($session_id) {
+                                    $query->where('session_id', $session_id);
+                                     })
+                                  ->get();
+        }
         return view('Backend.school_management.exam_routine.view_exam_routine',$data);
-	}  
+    }
 
     public function print(Request $request){
         $id= $request->input('examination_id');
--- resources/views/backend/school_management/exam_routine/index.blade.php
+++ resources/views/backend/school_management/exam_routine/index.blade.php
@@ -24,15 +24,25 @@
 
               <div class="col-md-12 mt-5 mb-5" style="border: 1px solid; padding: 10px">
                   <div class="row">
-                    <div class="col-md-11">
-                        <label class="form-control-label"><b>Exam title:</b></label>
+                    <div class="col-md-6">
+                        <label class="form-control-label"><b>Examination :</b></label>
                         <select class="form-control form-select select2" id="examination_id">
                             <option value="">Select Exam Title</option>
                             @foreach ($examinations as $examination)
                             <option value="{{ $examination->id }}">{{ $examination->name }}</option>
                             @endforeach
                         </select>
-                    </div>               
+                    </div> 
+                    
+                    <div class="col-md-6">
+                      <label class="form-control-label"><b>Session :</b></label>
+                      <select name="session_id" id="session_id" class="form-control form-select select2">
+                        <option value="0">Select Session</option>
+                        @foreach (@$sessions as $session)
+                            <option value="{{ $session->id }}">{{ @$session->start_year }} - {{ @$session->end_year }}</option>
+                        @endforeach
+                    </select>
+                  </div>    
                   </div>
               </div>
               <div class="get-exam-routine-show"></div>
@@ -46,18 +56,37 @@
 @section('script')
 
 <script>
-  $(document).on('change','#examination_id',function(e){
-    e.preventDefault();
-    let id = $(this).val();
-    console.log(id);
-    $.ajax({
-      type:'GET',
-      url:"{{ url('get-exam-routine') }}/"+id,
-      success:function(data){
-        $(".get-exam-routine-show").html(data);
-      }
+  // $(document).on('change','#examination_id',function(e){
+  //   e.preventDefault();
+  //   let id = $(this).val();
+  //   console.log(id);
+  //   $.ajax({
+  //     type:'GET',
+  //     url:"{{ url('get-exam-routine') }}/"+id,
+  //     success:function(data){
+  //       $(".get-exam-routine-show").html(data);
+  //     }
 
-    });
-  });
+  //   });
+  // });
+
+  $(document).on('change', '#examination_id, #session_id', function(e){
+    e.preventDefault();
+    let examination_id = $('#examination_id').val();
+    let session_id = $('#session_id').val();
+
+    console.log(examination_id,session_id);
+    
+    // Only proceed if both dropdowns have a selected value
+    if (examination_id && session_id) {
+        $.ajax({
+            type: 'GET',
+            url: "{{ url('get-exam-routine') }}/" + examination_id + "/" + session_id,
+            success: function(data) {
+                $(".get-exam-routine-show").html(data);
+            }
+        });
+    }
+});
 </script>
 @endsection
