241f56423e71a49b60e7f59a4d72d5966c5836a5
navieasoft <navieadevelopers@gmail.com>
2024-06-12 10:41:49 +0600

    ..

--- app/Http/Controllers/Backend/School_Management/Designation/DesignationController.php
+++ app/Http/Controllers/Backend/School_Management/Designation/DesignationController.php
@@ -9,7 +9,6 @@ use Illuminate\Support\Facades\DB;
 
 class DesignationController extends Controller
 {
-    // niaz
     public function index()
     {
         $data['designations'] = Designation::all();
